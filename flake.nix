bf79279887047f6897eff62804797f1ecbfb0ae8
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-08-18 11:40:14 +0300

    chore(nix): update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Timeful development shell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/0c2094806c9e542f31785ef3569ab9e900e3ce9c";
+    nixpkgs.url = "github:nixos/nixpkgs/f4b6996c4e8b9ee06ce147ec344c885f51071b14";
     systems.url = "github:nix-systems/default/future-26.11";
     flake-parts = {
       url = "github:hercules-ci/flake-parts/17c9d6cdfc60c64f4ee8d306f9bc0b4ccb51481e";

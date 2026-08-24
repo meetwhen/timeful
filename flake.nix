06d0b72278246d7d3e6bc7eea102779a02d81e7d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-08-24 17:36:25 +0300

    chore(nix): update nixpkgs to update graphify

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Timeful development shell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/f4b6996c4e8b9ee06ce147ec344c885f51071b14";
+    nixpkgs.url = "github:nixos/nixpkgs/c8f90650c15282fa8656a041bfbbd2403997a9a7";
     systems.url = "github:nix-systems/default/future-26.11";
     flake-parts = {
       url = "github:hercules-ci/flake-parts/17c9d6cdfc60c64f4ee8d306f9bc0b4ccb51481e";
